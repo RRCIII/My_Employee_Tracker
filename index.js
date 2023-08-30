d9d9ffb5f46850ecff61b78d5ec46e36bb939a9e
Ricky Carter <randolphcarter5@gmail.com>
2023-08-30 12:29:59 -0400

    added jest npm, as well as tested prompts in CLI

---
+++ index.js
@@ -0,0 +1,83 @@
+const inquirer = require('inquirer');
+
+// const ValidateInput = (input) => {
+//     if(input=== '') {
+//         return 'Must Add department';
+//     } return true;
+// } 
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What would you like to do?",
+            name: "addDepartment",
+            validate: (input) => {
+                if(input=== '') {
+                    return "Must Add department";
+                } return true;
+            } 
+        },
+        {
+            type:"input",
+            message: "What is the name of the department",
+            name: "departmentName"
+        },
+        {
+            type:"input",
+            message: "What is the name of the role?",
+            name: "nameRole"
+        },
+        {
+            type: "input",
+            message: "What is the salary of the role?",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "Which department does the role belong to?",
+            name: "department-role"
+        },
+        {
+            type: "input",
+            message:"What would you like to do?",
+            name: "Add-emp"
+        },
+        {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message:"what is the employee's last name?",
+            name: "lastName"
+        },
+        {
+            type:"input",
+            message: "What is the employee's role?",
+            name: 'empRole'
+        },
+        {
+            type: "input",
+            message:"Who is the employee's manager?",
+            name: "epmMang"
+        },
+        {
+            type: "input",
+            message:"What would you like to do?",
+            name: "updateEmpRole"
+        },
+        {
+            type: "list",
+            message:"Which employee's role would you like to update?",
+            name:"empChoices"
+        }
+    ]) 
+    
+    .then(res => {
+       console.log(`You've entered ${res}`)
+    });
+
+
+
