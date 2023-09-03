3fcb6c71dd311f3dbe064e3f3ea422683a7eec94
Ricky Carter <randolphcarter5@gmail.com>
2023-09-03 13:05:01 -0400

    created new DB with tables/foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,33 @@
-DROP DATABASE IF EXISTS department_data;
-CREATE DATABASE IF NOT EXISTS department_data ;
+DROP DATABASE IF EXISTS employee_tracker_data;
+CREATE DATABASE IF NOT EXISTS employee_tracker_data ;
+USE employee_tracker_data;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT, 
+    name VACHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,0),
+    department_id INT, 
+    PRIMARY KEY(id),
+    FOREING KEY (department_id) REFERNCE department_id
+);
+
+CREAT TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERNCE role(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
+
 
 
 
