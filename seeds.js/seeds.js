0e43d0d665e3d682023b3eca0137326f7465c1aa
Ricky Carter <randolphcarter5@gmail.com>
2023-09-03 13:14:43 -0400

    deleted and added seeds folder

---
+++ seeds.js/seeds.js
@@ -0,0 +1,13 @@
+INSERT INTO department (name)
+VALUES  ("Information Technology"),
+        ("Human Resources"),
+        ("Marketing"),
+        ("Sales"),
+        ("Finance");
+
+INSERT INTO role ( title, salary, department_id)
+VALUE ("Software Engineer, 200000.00, 1")
+      ("HR Manager, 100000.00, 2")
+      ("Marketing Analyist, 80000.00, 3")
+      ("Sales ")
+      ("")
