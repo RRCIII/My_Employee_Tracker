a3f697ba39a7f37fbde7c414456bcb0836087582
Ricky Carter <randolphcarter5@gmail.com>
2023-09-03 14:27:50 -0400

    deleted folders to restructure code, added prompts and queries including try/catch c async/await method

--- src/index.js
+++ src/index.js
@@ -11,5 +11,71 @@ const pool = mysql
         database: "employee_tracker_db"
     })
     .promise();
+// "View All Departments" 
+const viewAllDepartments = async () => {
+    const [rows] = await pool.query("SELECT * FROM department;");
+    console.table(rows)
+};
 
-    
+// "View All Roles"
+const viewAllRoles = async () => {
+    let query = `
+    SELECT r.id,
+           r.title,
+           r.salary,
+           r.department_id,
+           d.name
+    FROM role r
+    JOIN department d ON r.department_id = d.id;
+    `;
+
+    const [rows] = await pool.query(query);
+    console.table(rows);
+};
+
+//"View All Employess"
+const viewAllEmployees = async () =>{
+    let query = ` 
+      SELECT e.id,
+             e.first_name,
+             e.last_name,
+             e.manager_id,
+             r.title,
+             r.salary,
+             d.name
+        FROM employee e
+        JOIN role r ON e.role_id = r.id
+        JOIN department dON r.department_id = d.id
+        ORDER BY e.id; 
+        `;
+    const [rows] =await pool.query(query);
+    console.table(rows);
+};
+
+// "Add a department"
+const addDepartment = async () => {
+    try {
+        const dept = await inquirer.prompt({
+            name: "deptAdded",
+            type: "inout",
+            message: "Name of the department:",
+            valdiate: (name) => {
+                return name 
+                ? true
+                : console.log("Please enter a name for the department:", false);
+            },
+        });
+        const { deptAdded } = dept;
+
+        await pool.query(
+            `
+            INSERT INTO department (name)
+            VALUES (?)`,
+            [deptAdded]
+        );
+        return viewAllDepartments();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
