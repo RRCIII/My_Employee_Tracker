1804677d67f07f0397fe40bff3757932a51bfe33
Ricky Carter <randolphcarter5@gmail.com>
2023-09-06 15:57:14 -0400

    updating file structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 //  Node packages: inquirer@8.2.4 & mysql2
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+require('dotenv').config();
 
 // created a connection pool w/ mysql
 const pool = mysql
     .createPool({
         host: "127.0.0.1",
-        user: "root",
-        password: "",
-        database: "employee_tracker_db"
+        user: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     })
     .promise();
 
