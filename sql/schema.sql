bac21975606c991fdd6c997e80b939abdbccc49a
Ricky Carter <randolphcarter5@gmail.com>
2023-09-06 16:36:46 -0400

    updated folders

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
 -- database created 
 DROP DATABASE IF EXISTS employee_tracker_data;
-CREATE DATABASE IF NOT EXISTS employee_tracker_data ;
+CREATE DATABASE IF NOT EXISTS employee_tracker_data;
 USE employee_tracker_data;
 
 -- tables created for department, role, and employee 
@@ -15,19 +15,19 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL(10,0),
     department_id INT, 
-    PRIMARY KEY(id),
-    FOREING KEY (department_id) REFERNCE department_id
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREAT TABLE employee (
+CREATE TABLE employee (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT, 
     manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERNCE role(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 
